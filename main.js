08744a6a2051dccca91fc0ae4b2e8c50ae1282cd
Yuto Tamura <cayt1224@gmail.com>
2023-10-09 15:38:47 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,15 @@
+var boolean = true;
+if (boolean === true) {
+  console.log(boolean);
+};
+
+var num = 100;
+if (num === 100) {
+  console.log(num);
+};
+
+var text = "JavaScript";
+if (text === "JavaScript") {
+  console.log(text);
+};
+
